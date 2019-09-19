f2098102e6b58a88b8fbb9b656c758ba59b8f6c5
Dino Keylas <dinokeylas@gmail.com>
2019-09-19 10:59:37 +0700

    Finish UI of Bottom Navigation Bar

--- app/src/main/java/com/dinokeylas/melijoonline/HomeActivity.kt
+++ app/src/main/java/com/dinokeylas/melijoonline/HomeActivity.kt
@@ -1,16 +1,14 @@
 package com.dinokeylas.melijoonline
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
 
-class LoginActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-    }
+        setContentView(R.layout.activity_home)
 
+        
+    }
 }
