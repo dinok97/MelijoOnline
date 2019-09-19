db4fbe6c98cd0b7c9f2ad8e0e0bc34ceda5b0de3
Dino Keylas <dinokeylas@gmail.com>
2019-09-19 14:12:30 +0700

    - Create fragment Home, Trolley and Account - Add interaction between fragment using bottom navigation view

--- app/src/main/java/com/dinokeylas/melijoonline/HomeActivity.kt
+++ app/src/main/java/com/dinokeylas/melijoonline/HomeActivity.kt
@@ -2,6 +2,10 @@ package com.dinokeylas.melijoonline
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
 
@@ -9,6 +13,29 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        
+        val menu: Menu = findViewById<BottomNavigationView>(R.id.bottom_navigation).menu
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        // initialize first fragment that appear
+        selectedMenu(menu.getItem(0))
+
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            item: MenuItem -> selectedMenu(item)
+            false
+        }
+
+    }
+
+    private fun selectedMenu(menuItem: MenuItem){
+        menuItem.isChecked = true
+        when(menuItem.itemId){
+            R.id.navigation_home -> showFragment(HomeFragment())
+            R.id.navigation_trolley -> showFragment(TrolleyFragment())
+            R.id.navigation_account -> showFragment(AccountFragment())
+        }
+    }
+
+    private fun showFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
     }
 }
---
+++ app/src/main/java/com/dinokeylas/melijoonline/TrolleyFragment.kt
@@ -0,0 +1,24 @@
+package com.dinokeylas.melijoonline
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class TrolleyFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view: View = inflater.inflate(R.layout.fragment_trolley, container, false)
+        return view
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance():Fragment{
+            return TrolleyFragment()
+        }
+    }
+}
