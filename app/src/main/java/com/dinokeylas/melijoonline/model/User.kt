f146327accb04b3605c736559d9385c0b040a226
Dino Keylas <dinokeylas@gmail.com>
2019-10-18 13:24:51 +0700

    give default value to all attribute, so the data class user can binding with documentSnapshot from firebase

--- app/src/main/java/com/dinokeylas/melijoonline/model/User.kt
+++ app/src/main/java/com/dinokeylas/melijoonline/model/User.kt
@@ -1,11 +1,11 @@
 package com.dinokeylas.melijoonline.model
 
 data class User(
-    val userName: String,
-    val fullName: String,
-    val email: String,
-    val address: String,
-    val phoneNumber: String,
-    val profileImageUrl: String,
-    val password: String
+    val userName: String = "",
+    val fullName: String = "",
+    val email: String = "",
+    val address: String = "",
+    val phoneNumber: String = "",
+    val profileImageUrl: String = "",
+    val password: String = ""
     )
