7b36ca8ce58fec372b86f2b65c969ad5fc480f4d
Dino Keylas <dinokeylas@gmail.com>
2019-11-30 21:40:13 +0700

    create vegetableActivity

---
+++ app/src/main/java/com/dinokeylas/melijoonline/VegetableActivity.kt
@@ -0,0 +1,12 @@
+package com.dinokeylas.melijoonline
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VegetableActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vegetable)
+    }
+}
