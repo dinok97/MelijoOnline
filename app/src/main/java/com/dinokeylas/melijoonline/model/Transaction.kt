f34896bdd2cdcac79ca190c2efbb7f90fb2c816b
Dino Keylas <dinokeylas@gmail.com>
2019-12-19 23:14:35 +0700

    update transaction model: transactionId form val to var

--- app/src/main/java/com/dinokeylas/melijoonline/model/Transaction.kt
+++ app/src/main/java/com/dinokeylas/melijoonline/model/Transaction.kt
@@ -3,20 +3,20 @@ package com.dinokeylas.melijoonline.model
 import com.google.firebase.firestore.Exclude
 import java.util.*
 
-data class Transaction (
-    @get:Exclude val transactionId: String,
-    val transactionCode: String,
-    val userId: String,
-    val userEmail: String,
-    val date: Date,
-    val itemName: String,
-    val itemPrise: Int,
-    val imageUrl: String,
-    val userLocation: String,
-    val itemQty: Int,
-    val totalPay: Int,
-    val sellerName: String,
-    val done: Boolean,
-    val transactionProgress: String
+data class Transaction(
+    @get:Exclude var transactionId: String = "",
+    val transactionCode: String = "",
+    val userId: String = "",
+    val userEmail: String = "",
+    val date: Date = Calendar.getInstance().time,
+    val itemName: String = "",
+    val itemPrise: Int = 0,
+    val imageUrl: String = "",
+    val userLocation: String = "",
+    val itemQty: Int = 0,
+    val totalPay: Int = 0,
+    val sellerName: String = "",
+    val done: Boolean = false,
+    val transactionProgress: String = ""
 )
 
