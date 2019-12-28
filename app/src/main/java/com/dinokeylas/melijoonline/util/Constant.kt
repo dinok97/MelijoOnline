8d41f72fd6ba55bf99f75c973eef00a2950ee436
Dino Keylas <dinokeylas@gmail.com>
2019-12-28 22:07:54 +0700

    adjust because Item data class

--- app/src/main/java/com/dinokeylas/melijoonline/util/Constant.kt
+++ app/src/main/java/com/dinokeylas/melijoonline/util/Constant.kt
@@ -3,15 +3,25 @@ package com.dinokeylas.melijoonline.util
 interface Constant {
 
     interface Collection {
-
         companion object{
             val USER = "user"
             val VEGETABLE = "vegetable"
             val SEASONING = "seasoning"
             val VEGETABLE_PACKAGE = "vegetablePackage"
             val TRANSACTION = "transaction"
+            val ITEM = "item"
         }
+    }
 
+    interface Category {
+        companion object{
+            val VEGETABLE = "Vegetable"
+            val SEASONING = "Seasoning"
+            val VEGETABLE_PACKAGE = "VegetablePackage"
+            val SEAFOOD = "Seafood"
+            val MEET = "Meet"
+            val FRUIT = "Fruit"
+        }
     }
 
 }
