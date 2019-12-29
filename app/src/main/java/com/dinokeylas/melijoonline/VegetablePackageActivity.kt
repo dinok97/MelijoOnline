bb28e72a94e0d4d494c377f1d58f61d20a7804ef
Dino Keylas <dinokeylas@gmail.com>
2019-12-29 10:30:45 +0700

    add VegetablePackage module

---
+++ app/src/main/java/com/dinokeylas/melijoonline/VegetablePackageActivity.kt
@@ -0,0 +1,61 @@
+package com.dinokeylas.melijoonline
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.dinokeylas.melijoonline.adapter.ItemAdapter
+import com.dinokeylas.melijoonline.contract.VegetablePackageContract
+import com.dinokeylas.melijoonline.model.Item
+import com.dinokeylas.melijoonline.presenter.VegetablePackagePresenter
+import com.dinokeylas.melijoonline.util.GridItemDecoration
+import kotlinx.android.synthetic.main.activity_sea_food.*
+import java.util.*
+import kotlin.collections.ArrayList
+
+class VegetablePackageActivity : AppCompatActivity(), VegetablePackageContract.View {
+
+    private lateinit var vegetablePackagePresenter: VegetablePackagePresenter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vegetable_package)
+
+        vegetablePackagePresenter = VegetablePackagePresenter(this)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        Objects.requireNonNull(supportActionBar)?.title = "Daftar Paket Sayur"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onDataLoaded(itemList: ArrayList<Item>) {
+        val recyclerView: RecyclerView = findViewById(R.id.rv_item_list)
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
+        recyclerView.addItemDecoration(GridItemDecoration(10, 2))
+
+        val adapter = ItemAdapter(this, itemList)
+        recyclerView.adapter = adapter
+    }
+
+    override fun showProgressBar() {
+        progress_bar.visibility = View.VISIBLE
+    }
+
+    override fun hideProgressBar() {
+        progress_bar.visibility = View.GONE
+    }
+
+    override fun onBackPressed(){
+        super.onBackPressed()
+        startActivity(Intent(this, HomeActivity::class.java))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+}
---
+++ app/src/main/java/com/dinokeylas/melijoonline/presenter/VegetablePackagePresenter.kt
@@ -0,0 +1,35 @@
+package com.dinokeylas.melijoonline.presenter
+
+import android.util.Log
+import com.dinokeylas.melijoonline.contract.VegetablePackageContract
+import com.dinokeylas.melijoonline.model.Item
+import com.dinokeylas.melijoonline.util.Constant.Category.Companion.VEGETABLE_PACKAGE
+import com.dinokeylas.melijoonline.util.Constant.Collection.Companion.ITEM
+import com.google.firebase.firestore.FirebaseFirestore
+
+class VegetablePackagePresenter(_view: VegetablePackageContract.View): VegetablePackageContract.Presenter {
+
+    private val view: VegetablePackageContract.View = _view
+
+    init {
+        loadVegetablePackageData()
+    }
+
+    override fun loadVegetablePackageData() {
+        view.showProgressBar()
+        val itemList = ArrayList<Item>()
+        val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+        db.collection(ITEM).whereEqualTo("category", VEGETABLE_PACKAGE).get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    val itemSell: Item? = document.toObject(Item::class.java)
+                    itemList.add(itemSell!!)
+                    Log.d("DATA", itemSell.toString())
+                }
+                view.onDataLoaded(itemList)
+                view.hideProgressBar()
+            }.addOnFailureListener {
+                Log.d("LOG", "data gagal diambil")
+            }
+    }
+}
