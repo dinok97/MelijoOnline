15b52a12caa448b9983653b293f489584e3a6ffa
Dino Keylas <dinokeylas@gmail.com>
2020-04-05 13:28:25 +0700

    Add 5 field in Item dataClass: unitSale, createAt, updateAt, discount, isAvailable

--- app/src/main/java/com/dinokeylas/melijoonline/model/Item.kt
+++ app/src/main/java/com/dinokeylas/melijoonline/model/Item.kt
@@ -1,13 +1,19 @@
 package com.dinokeylas.melijoonline.model
 
 import com.google.firebase.firestore.Exclude
+import java.util.*
 
 data class Item (
     @get:Exclude var itemId: String = "",
     val name: String = "",
     val category: String = "",
     val price: Int = 0,
+    val unitSale: String = "",
     val description: String = "",
+    val createAt: Date = Calendar.getInstance().time,
+    val updateAt: Date = Calendar.getInstance().time,
+    val isAvailable: Boolean = false,
+    val discount: Int = 0,
     val sellerName: String = "",
     val imageUrl: String = ""
 )
